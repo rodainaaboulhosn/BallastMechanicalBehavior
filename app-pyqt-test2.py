4a519f2faaf5df078071ca5eb9d2b4df456cc302
rodainaaboulhosn <rodainaaboulhosn@gmail.com>
2026-06-04 18:17:02 +0300

    Update app-pyqt-test2.py

--- app-pyqt-test2.py
+++ app-pyqt-test2.py
@@ -322,6 +322,8 @@ if run:
         ax1.set_xlabel("Axial strain (%)")
         ax1.set_ylabel("Stress (kPa)")
         ax1.grid(True, linestyle="--")
+        ax1.set_xlim(left=0)
+        ax1.set_ylim(bottom=0)
         st.pyplot(fig1)
 
     with col2:
@@ -331,6 +333,8 @@ if run:
         ax2.set_xlabel("Axial strain (%)")
         ax2.set_ylabel("Volumetric deformation (%)")
         ax2.invert_yaxis()
+        ax2.set_xlim(left=0)
+        ax2.set_ylim(bottom=0)
         ax2.grid(True, linestyle="--")
         st.pyplot(fig2)
 
